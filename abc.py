95ebbbda690e5376d34d3c4a6a045da3807bbbaf
ShinHa_ <ymkzz123@gmail.com>
2026-04-03 13:10:34 +0900

    omg this is ez s2

--- abc.py
+++ abc.py
@@ -1,5 +1,15 @@
-print("physic professor stupid")
-#정창욱시발새끼야
-#credential check
-#your mom
-#please no more test
+def dfs(v):
+    visited[v] = True
+    print(v, end=' ')    
+    for i in g[v]:
+        if not visited[i]:
+            dfs(i)
+            
+            
+N = int(input())
+visited = [False]*(N+1)
+g = [list(map(int, input().split())) for _ in range(N)]
+res = 0
+
+
+dfs(1)
